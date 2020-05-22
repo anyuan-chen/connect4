d193b21195014497e84b86b42c829de08131f804
Andrew Chen <54372234+dishearten@users.noreply.github.com>
2020-05-22 11:53:52 -0400

    main menu basic interface

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,8 +1,36 @@
 package game;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Main extends JFrame{
+    //creation of panels to organize the main menu for the frame
+    JPanel title = new JPanel();
+    JPanel selection = new JPanel();
+    //title message
+    JLabel titleLabel = new JLabel("Welcome to Connect 4");
+    //buttons to enter each specific mode
+    JButton twoPlayer = new JButton("Two Player");
+    JButton easyAI = new JButton("Singleplayer - Easy");
+    JButton hardAI = new JButton("Singleplayer - Hard");
 
-public class Main {
-
+    public Main (){
+        //adds title label to the titlebar
+        title.add(titleLabel);
+        //adds mode selection buttons to the specified jpanel
+        selection.add(twoPlayer);
+        selection.add(easyAI);
+        selection.add(hardAI);
+        //adds creates layout to have the two panels stacked on top of each other
+        setLayout(new GridLayout(2,1));
+        //adds panels to the frame
+        add(title);
+        add(selection);
+        //sets size of the application
+        setSize(600,600);
+        setVisible(true);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        //creates instance of the application
+        Main m = new Main();
     }
 }

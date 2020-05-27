d9c359ced9fbc1c2e06412ae9241124c1727178c
fui-gao <65310940+fui-gao@users.noreply.github.com>
2020-05-27 14:37:07 -0400

    fixed difficulty bug

--- src/game/Options.java
+++ src/game/Options.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Options {
+
     //the overall difficulty variable will always store either the currentDifficulty or currentModerateDifficulty variables once a game has been entered
     //the minimax difficulty is based on the overall difficulty variable
     //when the easy mode is pressed, the overall difficulty variable changes into the currentEasyDifficulty variable while if the hard mode is pressed it will change into the currentModerate difficulty
@@ -17,7 +18,8 @@ public class Options {
     //default maximum move amount allowed
     public static int moveAmount = 42;
     JFrame options = new JFrame();
-    public Options(){
+
+    public Options() {
         //label selectors
         JLabel colorLabel = new JLabel("Please select the color for player 1:");
         JLabel color2Label = new JLabel("Please select the color for player 2/AI");
@@ -26,42 +28,42 @@ public class Options {
 
         //options available in the combo box for colors and difficulty (depth of minmax ai)
         String[] colors = {"BLACK", "ORANGE", "GREEN", "YELLOW", "RED"};
-        Integer [] difficulty = {1,2,3,4,5};
+        Integer[] difficulty = {1, 2, 3, 4, 5};
 
         /**
-         * Reason a method was not used for the repetitive code below is because they are of different data types
-         * and all have specific checks in them. (eg. one must add .png to the color name to access the file
+         * Reason a method was not used for the repetitive code below is because
+         * they are of different data types and all have specific checks in
+         * them. (eg. one must add .png to the color name to access the file
          */
-
         //creation of new combobox for the user to select
         JComboBox<String> player1Color = new JComboBox<String>(colors);
         //selects the current value that the user has enabled for them
-        for (int i = 0; i < colors.length; i++){
-            if ((colors[i]+".png").equals(player1CurrentColor)){
+        for (int i = 0; i < colors.length; i++) {
+            if ((colors[i] + ".png").equals(player1CurrentColor)) {
                 player1Color.setSelectedIndex(i);
             }
         }
         //creation of new combobox for user
         JComboBox<String> player2Color = new JComboBox<String>(colors);
         //selects current value
-        for (int i = 0; i < colors.length; i++){
-            if ((colors[i]+".png").equals(player2CurrentColor)){
+        for (int i = 0; i < colors.length; i++) {
+            if ((colors[i] + ".png").equals(player2CurrentColor)) {
                 player2Color.setSelectedIndex(i);
             }
         }
         //creation of new combobox for user
         JComboBox<Integer> aiDifficulty = new JComboBox<Integer>(difficulty);
         //selects current value
-        for (int i = 0; i < difficulty.length; i++){
-            if (difficulty[i] == currentEasyDifficulty){
+        for (int i = 0; i < difficulty.length; i++) {
+            if (difficulty[i] == currentEasyDifficulty) {
                 aiDifficulty.setSelectedIndex(i);
             }
         }
         //creation of new combobox for user
         JComboBox<Integer> ai2Difficulty = new JComboBox<Integer>(difficulty);
         //selects current value
-        for (int i = 0; i < difficulty.length; i++){
-            if (difficulty[i] == currentModerateDifficulty){
+        for (int i = 0; i < difficulty.length; i++) {
+            if (difficulty[i] == currentModerateDifficulty) {
                 ai2Difficulty.setSelectedIndex(i);
             }
         }
@@ -71,36 +73,33 @@ public class Options {
 
         //following 5 blocks of code are adding the label and combobox for a respective to topic to a panel
         JPanel color1Panel = new JPanel();
-        colorLabel.setForeground(new Color(228,255,255));
+        colorLabel.setForeground(new Color(228, 255, 255));
         color1Panel.add(colorLabel);
         color1Panel.add(player1Color);
-        color1Panel.setBackground(new Color(0,0,0,0));
+        color1Panel.setBackground(new Color(0, 0, 0, 0));
 
         JPanel color2Panel = new JPanel();
         color2Panel.add(color2Label);
-        color2Label.setForeground(new Color(228,255,255));
+        color2Label.setForeground(new Color(228, 255, 255));
         color2Panel.add(player2Color);
-        color2Panel.setBackground(new Color(0,0,0,0));
-
+        color2Panel.setBackground(new Color(0, 0, 0, 0));
 
         JPanel difficultyPanel = new JPanel();
-        difficultyLabel.setForeground(new Color(228,255,255));
+        difficultyLabel.setForeground(new Color(228, 255, 255));
         difficultyPanel.add(difficultyLabel);
         difficultyPanel.add(aiDifficulty);
-        difficultyPanel.setBackground(new Color(0,0,0,0));
-
+        difficultyPanel.setBackground(new Color(0, 0, 0, 0));
 
         JPanel difficulty2 = new JPanel();
-        difficultyLabelModerate.setForeground(new Color(228,255,255));
+        difficultyLabelModerate.setForeground(new Color(228, 255, 255));
         difficulty2.add(difficultyLabelModerate);
         difficulty2.add(ai2Difficulty);
-        difficulty2.setBackground(new Color(0,0,0,0));
-
+        difficulty2.setBackground(new Color(0, 0, 0, 0));
 
         JPanel maxNumberMoves = new JPanel();
         JLabel maxMovesLabel = new JLabel("Please select the max number of turns");
-        maxMovesLabel.setForeground(new Color(228,255,255));
-        maxNumberMoves.setBackground(new Color(0,0,0,0));
+        maxMovesLabel.setForeground(new Color(228, 255, 255));
+        maxNumberMoves.setBackground(new Color(0, 0, 0, 0));
         maxNumberMoves.add(maxMovesLabel);
         maxNumberMoves.add(moveEntry);
 
@@ -109,19 +108,18 @@ public class Options {
         //action listener using lambada function
         saveAndExit.addActionListener(
                 actionEvent -> {
-                    if (player1Color.getSelectedItem().equals(player2Color.getSelectedItem())){
+                    if (player1Color.getSelectedItem().equals(player2Color.getSelectedItem())) {
                         JOptionPane.showMessageDialog(new JFrame(), "Player 1 and 2 cannot have same color");
                         return;
                     }
                     try {
-                        if (Integer.parseInt(moveEntry.getText()) < 43){
+                        if (Integer.parseInt(moveEntry.getText()) < 43) {
                             moveAmount = Integer.parseInt(moveEntry.getText());
-                        }
-                        else{
+                        } else {
                             JOptionPane.showMessageDialog(new JFrame(), "There is a maximum of 42 moves in connect 4");
                             return;
                         }
-                    } catch (Exception e){
+                    } catch (Exception e) {
                         JOptionPane.showMessageDialog(new JFrame(), "Invalid amount of moves");
                         return;
                     }
@@ -129,21 +127,21 @@ public class Options {
                     player1CurrentColor = player1Color.getSelectedItem() + ".png";
                     player2CurrentColor = player2Color.getSelectedItem() + ".png";
                     currentEasyDifficulty = Integer.parseInt(String.valueOf(aiDifficulty.getSelectedItem())) + 1;
-                    currentModerateDifficulty = Integer.parseInt(String.valueOf(ai2Difficulty.getSelectedItem())) + 1;
+                    currentModerateDifficulty = Integer.parseInt(String.valueOf(ai2Difficulty.getSelectedItem())) + 2;
                     //makes it invisible so nothing can happen
                     options.setVisible(false);
                 }
         );
         //sets layout and adds all of the different panels needed
-        options.getContentPane().setBackground(new Color(22,31,59));
-        options.setLayout(new GridLayout(6,1));
+        options.getContentPane().setBackground(new Color(22, 31, 59));
+        options.setLayout(new GridLayout(6, 1));
         options.add(color1Panel);
         options.add(color2Panel);
         options.add(difficultyPanel);
         options.add(difficulty2);
         options.add(maxNumberMoves);
         options.add(saveAndExit);
-        options.setSize(430,550);
+        options.setSize(430, 550);
         ImageIcon icon = new ImageIcon("./src/Assets/connect4icon.png");
         options.setIconImage(icon.getImage());
         options.setLocationRelativeTo(null);
